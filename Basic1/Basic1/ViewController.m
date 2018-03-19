4b3cbc65ca9d11792ff17d6d130aa3993cae745f
maohs <1543292509@qq.com>
2018-03-19 13:47:24 +0800

    update

--- Basic1/Basic1/ViewController.m
+++ Basic1/Basic1/ViewController.m
@@ -10,9 +10,10 @@
 #import "KCContact.h"
 #import "KCContactGroup.h"
 
-@interface ViewController ()<UITableViewDataSource> {
+@interface ViewController ()<UITableViewDataSource,UITableViewDelegate,UIAlertViewDelegate> {
     UITableView *_tableView;
-    NSMutableArray *_contacts;
+    NSMutableArray *_contacts;//联系人模型
+    NSIndexPath *_selectedIndexPath;//当前选中的组和行
 }
 
 @end
@@ -26,6 +27,7 @@
     [self initData];
     _tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStyleGrouped];
     _tableView.dataSource = self;
+    _tableView.delegate = self;
     [self.view addSubview:_tableView];
 }
 
@@ -79,7 +81,31 @@
     NSLog(@"生成单元格(组：%zi,行%zi)",indexPath.section,indexPath.row);
     KCContactGroup *group=_contacts[indexPath.section];
     KCContact *contact=group.contacts[indexPath.row];
-    UITableViewCell *cell=[[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
+
+    //由于此方法调用十分频繁，cell的标示声明成静态变量有利于性能优化
+    static NSString *cellIdentifier = @"UITableViewCellIdentifierKey1";
+    static NSString *cellIdentifierForFirstRow = @"UITableviewCellIdentifierKeyWithSwitch";
+    //首先根据标示去缓存池取
+    UITableViewCell *cell;
+    if (indexPath.row == 0) {
+        cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifierForFirstRow];
+    }else {
+        cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    }
+    if (!cell) {
+        if (indexPath.row == 0) {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:cellIdentifierForFirstRow];
+            UISwitch *sw = [[UISwitch alloc] init];
+            [sw addTarget:self action:@selector(switchValueChange:) forControlEvents:UIControlEventValueChanged];
+            cell.accessoryView = sw;
+        }else {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:cellIdentifier];
+            cell.accessoryType = UITableViewCellAccessoryDetailButton;
+        }
+    }
+    if (indexPath.row == 0) {
+        ((UISwitch *)cell.accessoryView).tag = indexPath.section;
+    }
     cell.textLabel.text=[contact getName];
     cell.detailTextLabel.text=contact.phoneNumber;
     return cell;
@@ -108,6 +134,59 @@
     return indexs;
 }
 
+#pragma mark 代理方法
+#pragma mark 设置分组标题内容高度
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    if (section == 0) {
+        return 50;
+    }
+    return 40;
+}
+#pragma mark 设置每行高度（每行高度可以不一样）
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 45;
+}
+#pragma mark 设置尾部说明内容高度
+-(CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
+    return 40;
+}
+#pragma mark 点击行
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    _selectedIndexPath = indexPath;
+    KCContactGroup *group = _contacts[indexPath.section];
+    KCContact *contact = group.contacts[indexPath.row];
+    //创建弹出窗口
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"System Info" message:[contact getName] delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"OK", nil];
+    alert.alertViewStyle = UIAlertViewStylePlainTextInput;
+    UITextField *textField = [alert textFieldAtIndex:0];
+    textField.text = contact.phoneNumber;
+    [alert show];//显示窗口
+}
+
+#pragma mark 窗口的代理方法，用户保存数据
+-(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    //当点击了第二个按钮(OK)
+    if (buttonIndex == 1) {
+        UITextField *textField = [alertView textFieldAtIndex:0];
+        //修改模型数据
+        KCContactGroup *group =_contacts[_selectedIndexPath.section];
+        KCContact *contact =group.contacts[_selectedIndexPath.row];
+        contact.phoneNumber = textField.text;
+        //刷新表格(不推荐使用)
+//        [_tableView reloadData];
+        //刷新表格
+        NSArray *indexPaths = @[_selectedIndexPath];
+        [_tableView reloadRowsAtIndexPaths:indexPaths withRowAnimation:UITableViewRowAnimationLeft];
+    }
+}
+#pragma mark 重写状态样式方法
+-(UIStatusBarStyle)preferredStatusBarStyle {
+    return UIStatusBarStyleLightContent;
+}
+#pragma mark 切换开关转化事件
+-(void)switchValueChange:(UISwitch *)sw {
+    NSLog(@"section:%zi,switch:%zi",sw.tag,sw.on);
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
